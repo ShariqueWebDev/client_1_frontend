56bc5a4626e82094ca8aed84ae95305197fd7001
sharique-aslam <shariquedev966@gmail.com>
2025-09-06 23:24:04 +0530

    changes in nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,33 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**", // allow all domains
-      },
-    ],
+    domains: ["res.cloudinary.com", "cdn.myshop.com", "localhost"],
   },
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "http://ec2-3-111-25-104.ap-south-1.compute.amazonaws.com:4000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "**", // allow all domains
+//       },
+//     ],
+//   },
+//   /* config options here */
+// };
+
+// export default nextConfig;
