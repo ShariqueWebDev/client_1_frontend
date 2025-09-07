77da733f9080b8a12eb5fa22c6812b916528a2d3
sharique-aslam <shariquedev966@gmail.com>
2025-09-07 12:10:38 +0530

    add refilly bakcend ssl

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -8,27 +9,10 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination:
-          "http://ec2-3-111-25-104.ap-south-1.compute.amazonaws.com:4000/api/:path*",
+        destination: "https://backend.refilly.in/api/:path*", // ✅ HTTPS + custom domain
       },
     ];
   },
 };
 
 export default nextConfig;
-
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "**", // allow all domains
-//       },
-//     ],
-//   },
-//   /* config options here */
-// };
-
-// export default nextConfig;
